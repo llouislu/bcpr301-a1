25efc944cfd91ff5e20f16e8702e43771913f545
larsenlouis <louis.dev@outlook.com>
2019-08-22 13:44:47 +1200

    add syntax error handling for filesourcereader

--- tigr/lib/source_reader/file_source_reader.py
+++ tigr/lib/source_reader/file_source_reader.py
@@ -2,8 +2,11 @@ from tigr.lib.interface import AbstractSourceReader
 
 class FileSourceReader(AbstractSourceReader):
     def go(self):
-        for line in self.file_name:
+        for line_number, line in enumerate(self.file_name, 1):
             line = line.strip()
             line = line.upper()
             self.source.append(line)
-        self.parser.parse(self.source)
+        try:
+            self.parser.parse(self.source)
+        except:
+            print('you have a syntax error on Line {}: "{}"'.format(line_number, line))
