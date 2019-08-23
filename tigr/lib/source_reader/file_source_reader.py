1cd4c43dad706218fabe40df1f5803c01fc77ac6
larsenlouis <louis.dev@outlook.com>
2019-08-23 13:41:13 +1200

    let parser handle syntax error

--- tigr/lib/source_reader/file_source_reader.py
+++ tigr/lib/source_reader/file_source_reader.py
@@ -4,9 +4,5 @@ class FileSourceReader(AbstractSourceReader):
     def go(self):
         for line_number, line in enumerate(self.file_name, 1):
             line = line.strip()
-            line = line.upper()
             self.source.append(line)
-        try:
-            self.parser.parse(self.source)
-        except:
-            print('you have a syntax error on Line {}: "{}"'.format(line_number, line))
+        self.parser.parse(self.source)
